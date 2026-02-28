abbaa8f792189d9b5deb76842fbefa3f2eb3097d
Gleb Druzhinin <lizardgd99@gmail.com>
2026-03-01 00:13:38 +0300

    Обновить README для локального Git

---
+++ src/main/java/ru/mentee/power/NewFeature.java
@@ -0,0 +1,7 @@
+package ru.mentee.power;
+
+public class NewFeature {
+    public static void main (String[] args) {
+        System.out.println("feature/DVT-4... Текст для того, чтобы внести ветку в Pull Request");
+    }
+}
--- src/main/java/ru/mentee/power/ProgressDemo.java
+++ src/main/java/ru/mentee/power/ProgressDemo.java
@@ -2,13 +2,13 @@ package ru.mentee.power;
 
 public class ProgressDemo {
     public static void main(String[] args) {
-        var progress = new MenteeProgress("Gleb Druzhinin", 1, 7);
+        var progress = new MenteeProgress("Gleb Druzhinin", 2, 7);
         System.out.println(progress.summary());
         if (progress.readyForSprint()) {
             System.out.println("Status: sprint ready");
         } else {
             System.out.println("Status: backlog first");
         }
-        System.out.println("Текущая ветка Git: feature/DVT-3");
+        System.out.println("Текущая ветка Git: feature/DVT-4");
     }
 }
