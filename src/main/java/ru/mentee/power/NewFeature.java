907aae47bf6044d55c9824982139a56c739a27a0
Gleb Druzhinin <lizardgd99@gmail.com>
2026-03-03 21:56:55 +0300

    Удален NewFeature.java, так как задание DVT-4 требует только настройки Git/PR

--- src/main/java/ru/mentee/power/NewFeature.java
+++
@@ -1,7 +0,0 @@
-package ru.mentee.power;
-
-public class NewFeature {
-    static void main() {
-        System.out.println("feature/DVT-4... Текст для того, чтобы внести ветку в Pull Request");
-    }
-}
