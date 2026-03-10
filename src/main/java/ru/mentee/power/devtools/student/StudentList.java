dbbc41c69f85d2a8c71b5911d3039474796b1d05
Gleb Druzhinin <lizardgd99@gmail.com>
2026-03-11 00:35:36 +0300

    Fix Checkstyle violation

--- src/main/java/ru/mentee/power/devtools/student/StudentList.java
+++ src/main/java/ru/mentee/power/devtools/student/StudentList.java
@@ -15,7 +15,7 @@ class StudentList {
   private int temp3;
   private int temp4; // нарушение4: Лучше объявлять каждую на отдельной строке
 
-  private static final int Max_Size = 10; // нарушение: Должно быть MAX_SIZE
+  private static final int MAX_SIZE = 10; // нарушение: Должно быть MAX_SIZE
 
   // нарушение: имя метода
   public void addStudent(Student student) {
